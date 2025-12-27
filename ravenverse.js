2cecb409994704b3c24fc50c2bebc01ed75ee095
RAVEN <suzyoongi23@gmail.com>
2025-12-27 13:27:23 +0530

    added ravenverse.js

---
+++ ravenverse.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const card = document.querySelector(".profile-card");
+  if (!card) return;
+
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const rotateX = -(y - rect.height / 2) / 12;
+    const rotateY = (x - rect.width / 2) / 12;
+
+    card.style.transform = `
+      rotateX(${rotateX}deg)
+      rotateY(${rotateY}deg)
+      scale(1.03)
+    `;
+
+    // light follow
+    card.style.setProperty("--mx", `${(x / rect.width) * 100}%`);
+    card.style.setProperty("--my", `${(y / rect.height) * 100}%`);
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = `
+      rotateX(0deg)
+      rotateY(0deg)
+      scale(1)
+    `;
+    card.style.setProperty("--mx", "50%");
+    card.style.setProperty("--my", "50%");
+  });
+});
+
